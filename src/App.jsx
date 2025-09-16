7faf6b65e71a50d19b4dccab61bede163e96b058
Priyotush <priyotushpaul40@gmail.com>
2025-09-16 22:33:30 +0600

    counter app and input text show

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,34 @@ import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
 
   const resetHandle = () => {
     setCount(0);
   };
 
+  const inputHandle = (e) => {
+    console.log(e.target.value);
+    setText(e.target.value);
+  };
+
   return (
     <>
-      <h1>Counter App</h1>
-      <h1>{count}</h1>
-      <button onClick={() => setCount(count + 1)}>Increase</button>
-      <button onClick={() => setCount(count - 1)}>Increase</button>
-      <button onClick={resetHandle}>Reset</button>
+      <div className="container">
+        <h1>Practice Task 1: Simple Counter</h1>
+        <h1>{count}</h1>
+        <button onClick={() => setCount(count + 1)}>Increase</button>
+        <button onClick={() => count > 0 && setCount(count - 1)}>
+          Increase
+        </button>
+        <button onClick={resetHandle}>Reset</button>
+      </div>
+
+      <div className="container">
+        <h1>Practice Task 2: Input Mirror</h1>
+        <h2>You Typed : {text}</h2>
+        <input value={text} type="text" onChange={inputHandle} />
+      </div>
     </>
   );
 }
